9992fcabff0cf87c65a8fe665a3f92b89a46b4f0
xieyang <xieyang@memect.co>
2018-06-26 15:32:52 +0800

    [feature]Add examples about how to use express-authing

---
+++ examples/app.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+var authing = require('express-authing')
+
+app.use(authing({
+  clientId: '',
+  secret: ''
+}))
+
+app.get('/', function (req, res) {
+  //use authing
+
+  //req.authing.login
+  //req.authing.register
+})
+
+app.listen(3000)
