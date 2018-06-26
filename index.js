b80ae22540756fa02fced7b0f871e3834f1ee4a2
Helen <yinhelen.hlj@qq.com>
2018-06-26 14:19:48 +0800

    [feature]add authing express sdk

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const Authing = require('authing-js-sdk');
 module.exports = function(options) {
-          return function(req, res, next) {
-                      // Implement the middleware function based on the options object
-                          next()
-                            }
-}
+    return function(req, res, next) {
+        const auth = new Authing({
+            clientId: options.clientId,
+            secret: options.secret,
+        })
+        auth.then((validAuth) => {
+            req.authing = validAuth;
+            next()
+        }).catch((error) => {
+            throw error;
+        });
+    };
+};
